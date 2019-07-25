52fe4c074c5d2db0ef1cd966c8ff408cbeda0c2c
klc <272953087@qq.com>
2019-07-25 11:07:43 +0800

    启动流程实例

--- demo/src/test/java/com/activiti/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/activiti/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.activiti.demo;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +31,23 @@ public class DemoApplicationTests {
                 .deploy();
         System.out.println("部署流程ID： " + deployment.getId());
         System.out.println("流程名称：" + deployment.getName());
+    }
+
+    @Test
+    public void startProcessInstance() throws Exception {
+        // 流程定义的Key
+        String processDefinitionKey = "myProcess_1";
+
+        /**获取流程引擎*/
+        ProcessEngine processEngine = spec.buildProcessEngine();
+
+        /**RuntimeService，执行管理，包括启动、推进、删除流程实例等操作*/
+        /**使用流程定义的key启动流程实例，默认按照最新版本启动*/
+        ProcessInstance processInstance = processEngine.getRuntimeService()
+                .startProcessInstanceByKey(processDefinitionKey);
+        System.out.println("启动流程实例： " + processInstance.getId());
+        System.out.println("流程定义ID： " + processInstance.getProcessDefinitionId());
+
     }
 
     @Test
